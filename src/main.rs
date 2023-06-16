cfae9464005c146e8ab6f16b0bc0c4f2b040b45d
sunray-ley <sunray.ley@gmail.com>
2023-06-16 15:16:20 +0800

    feat: add guessing game demo

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,42 @@
 use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io::{self, stdout, BufWriter};
 
 fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                ferris_say("You win!");
+                break;
+            }
+        }
+    }
+}
+
+fn ferris_say(message: &str) {
     let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
